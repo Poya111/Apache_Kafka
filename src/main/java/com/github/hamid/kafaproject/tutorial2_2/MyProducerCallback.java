a235cbc8cdc0abd6d4961a033891b8f580d77e74
Poya <poyabagheri1350@gmail.com>
2020-08-18 01:27:09 -0400

    Synchronous & Asynchronous data stream

---
+++ src/main/java/com/github/hamid/kafaproject/tutorial2_2/MyProducerCallback.java
@@ -0,0 +1,17 @@
+package com.github.hamid.kafaproject.tutorial2_2;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class MyProducerCallback implements Callback {
+
+    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+        //Not Success Callback
+        if (e != null){
+            System.out.println("Asynchronous Unsuccessful Callback.");
+        }else{
+            System.out.println("Asynchronous Successful Callback.");
+        }
+
+    }
+}
